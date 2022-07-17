bc0501f9f5fc2bbd7929a0cdca60d9fb1b5cc3fe
slurpcat <m.wojtanek4567@gmail.com>
2022-07-17 18:09:12 +0200

    made the code better, added comments

--- graph.js
+++ graph.js
@@ -2,13 +2,13 @@ const TIME_INTERVAL = 0.002;
 var howManyTimesGenerated = 0;
 var myChart;
 
-function resizeCanvas(){
+function resizeCanvas(){ //resize the canvas based on window size
     var canvas = document.getElementById("myChart");
     canvas.width = window.innerWidth * 0.65;
     canvas.height = window.innerHeight * 0.9;
 }
 
-function generateRandom(min = -1, max = 1){
+function generateRandom(min = -1, max = 1){ //generate a random decimal between -1 and 1
     let difference = max - min;
     let rand = Math.random();
     rand = rand * difference;
@@ -16,10 +16,10 @@ function generateRandom(min = -1, max = 1){
     return rand;
 }
 
-function normalRand(){
+function normalRandom(){ //generate a random decimal from a normal distribution of size x. its not perfect
     x = 10;
     res = 0;
-    for(ii=0;ii<x;ii++){
+    for(let i=0;i<x;i++){
         res += generateRandom(-1,1);
     }
     return(res/x)
@@ -27,26 +27,20 @@ function normalRand(){
 
 function generateData(sP,eRoR,eAV){
     
-    data = {}
-    data[0] = sP
+    priceData = new Array;
+    priceData[0] = sP;
 
-    for(i=1;i<howLong;i++){
-        data[i] = data[i-1] * Math.exp((eRoR - eAV**2 / 2) * TIME_INTERVAL + eAV * normalRand() * Math.sqrt(TIME_INTERVAL));
-        data[i] = data[i].toFixed(2);
+    for(let i=1;i<howLong;i++){
+        priceData[i] = priceData[i-1] * Math.exp((eRoR - eAV**2 / 2) * TIME_INTERVAL + eAV * normalRandom() * Math.sqrt(TIME_INTERVAL));
+        priceData[i] = priceData[i].toFixed(2);
     }
-    
-    dataArray = new Array;
-    for(i=0;i<howLong;i++){
-        dataArray[i] = Object.values(data)[i];
-    }
-
-    return (dataArray);
+    return (priceData);
 }
 
 function generateChart(){
 
-    howManyTimesGenerated += 1;
-    if(howManyTimesGenerated>1){
+    howManyTimesGenerated += 1;   
+    if(howManyTimesGenerated>1){ // if this isnt the first chart generated, "destroy" the previous one (removes the ghost lines)
         myChart.destroy();  
     }
 
@@ -57,12 +51,20 @@ function generateChart(){
     sP = Number(document.getElementById("sP").value);
     eRoR = Number(document.getElementById("eRoR").value);
     eAV = Number(document.getElementById("eAV").value);
+    drawPointsCheck = document.getElementById("drawPointsCheck").checked;
+
+    let pointRadius;
+    if(drawPointsCheck){
+        pointRadius = 3;
+    }else{
+        pointRadius = 0;
+    }
 
     datasets = new Array;
 
     var r,g,b;
-    for(n=0;n<howMany;n++){
-        if(howMany>1){
+    for(let i=0;i<howMany;i++){  
+        if(howMany>1){ // if only 1 graph is generated, make it black. otherwise randomise colors
             r = Math.round(Math.random() * 255);
             g = Math.round(Math.random() * 255);
             b = Math.round(Math.random() * 255);
@@ -70,9 +72,8 @@ function generateChart(){
             r = g = b = 0;
         }
 
-
-        datasets[n] = {
-            label: "Line " + (Number(n+1)).toString(),
+        datasets[i] = {  // generate and add new datasets to the array
+            label: i+1,
             data: generateData(sP,eRoR,eAV),
             fill: false,
             borderColor: 'rgba(' + r + ', ' + g + ", " + b + ')',
@@ -91,9 +92,18 @@ function generateChart(){
         options: {
             maintainAspectRatio: true,
             responsive: false,
-            tension: 0
-        }
+            tension: 0,
+            parsing: false,
+            normalized: true,
+            spanGaps: true,
+            animation: false,
+
+            elements: {
+                point: {
+                    radius: pointRadius
+                }
+            }
+        }       
     }
-    
     myChart = new Chart(CTX, CONFIG);
 }
